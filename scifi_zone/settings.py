237a8255b922c337a1144c9aff86e7fb0d90b42c
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-03 21:36:58 +0100

    feat: Create home app and add index.html

--- scifi_zone/settings.py
+++ scifi_zone/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
     # OTHER
     'crispy_forms',
@@ -69,7 +70,10 @@ ROOT_URLCONF = 'scifi_zone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
