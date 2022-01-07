171992954dbf4b95de0ec5e5cbe477666c3352ac
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-07 22:41:37 +0100

    feat: Add view for ticket_detail.html

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Ticket
+from django.shortcuts import render, get_object_or_404
+from .models import Ticket, Actor
 
 def all_tickets(request):
     """ A view to show all tickets """
@@ -11,3 +11,22 @@ def all_tickets(request):
     }
 
     return render(request, 'tickets/tickets.html', context)
+
+
+def ticket_detail(request, tickets_id, actors_id=3):
+    """ A view to show detailed ticket information """
+
+    ticket = get_object_or_404(Ticket, pk=tickets_id)
+
+    goodies = ticket.included.split(',')
+    actors = Actor.objects.all()
+    actor = get_object_or_404(Actor, pk=actors_id)
+
+    context = {
+        'ticket': ticket,
+        'goodies': goodies,
+        'actors': actors,
+        'actor': actor,
+    }
+
+    return render(request, 'tickets/ticket_detail.html', context)
