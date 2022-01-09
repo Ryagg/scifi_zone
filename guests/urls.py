076af25161dc8df787e382578afa42b2134c6914
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-09 22:00:52 +0100

    feat: Add basic guest_detail template
    Update urls.py and views.py in guests app

--- guests/urls.py
+++ guests/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_all_guests, name='guests'),
+    path('<int:actors_id>', views.guest_detail, name='guest_detail'),
 
 ]
--- guests/views.py
+++ guests/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from tickets.models import Actor
+from django.shortcuts import render, get_object_or_404
+from tickets.models import Actor, Ticket
 
 def view_all_guests(request):
     """A view to show all guests holding panels at the convention"""
@@ -11,3 +11,22 @@ def view_all_guests(request):
     }
 
     return render(request, 'guests/guests.html', context)
+
+
+def guest_detail(request, actors_id):
+    """ A view to show detailed actor information """
+
+    actors = Actor.objects.all()
+    actor = get_object_or_404(Actor, pk=actors_id)
+    tickets = Ticket.objects.all()
+    autograph_price_category = f'Autograph Ticket Price Category {actor.star_autograph_category}'
+
+    context = {
+        'actors': actors,
+        'actor': actor,
+        'tickets': tickets,
+        'autograph_price_category': autograph_price_category,
+    }
+    print(autograph_price_category)
+    return render(
+        request, 'guests/guest_detail.html', context)
