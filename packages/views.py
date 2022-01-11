7d58cd0811abd30f55264f0224b1241fd969caa9
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-11 22:43:08 +0100

    feat: Add packages view + template
    Add URL to home template

--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Package
 
-# Create your views here.
+def packages(request):
+    """ A view to show all package categories """
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'packages/packages.html', context)
