f1a5c25068f7f20bfbf16b275aba62fa00e49358
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-22 00:50:58 +0100

    refactor: Remove app packages

--- packages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.packages, name='packages'),
-    path('<int:categories_id>/', views.package_detail, name="package_detail")
-]
