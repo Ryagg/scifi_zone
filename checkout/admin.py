235447845c6467e91ffa6e3247e0b9e4d0f766c1
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-23 03:15:03 +0100

    feat: Add checkout admin models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ("lineitem_total",)
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ("order_number", "date", "grand_total")
+
+    fields = (
+        "order_number",
+        "date",
+        "full_name",
+        "email",
+        "sub_total",
+        "grand_total",
+        "street_address1",
+        "street_address2",
+        "city",
+        "state",
+        "postcode",
+        "country",
+    )
+
+    # restrict columns that show up in the order list
+    list_display = ("order_number", "date", "full_name", "email", "grand_total")
+
+    # use reverse chronological order
+    ordering = "-date"
+
+
+admin.site.register(Order, OrderAdmin)
