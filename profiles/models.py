fbfb99ee3167391e36a9c027cf10b16355a6d4ee
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-01-30 18:54:25 +0100

    refactor: Re-arrange fields in profiles' model

--- profiles/models.py
+++ profiles/models.py
@@ -10,14 +10,14 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
-    delivery information and order history
+    billing information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_city = models.CharField(max_length=40, null=True, blank=True)
-    default_state = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_state = models.CharField(max_length=80, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
