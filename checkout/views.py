c21544f95d51c416c5fbef52a5a9f41175fbcd24
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-02-06 10:24:24 +0100

    refactor: Add decorators
    Format file with autopep8

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
 import json
-from django.shortcuts import render, redirect, reverse,get_object_or_404, HttpResponse
-from django.views.decorators.http import require_POST
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
+# from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
 import stripe
@@ -11,38 +13,39 @@ from profiles.forms import UserProfileForm
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 
+# pylint: disable=locally-disabled, no-member, using-constant-test
 
 
-# pylint: disable=locally-disabled, no-member
+# @require_POST
+# def cache_checkout_data(request):
+#     try:
+#         # get payment intent id
+#         pid = request.POST.get('client_secret').split('_secret')[0]
+#         # set up stripe
+#         stripe.api_key = settings.STRIPE_SECRET_KEY
+#         # tell stripe what we want to modify
+#         stripe.PaymentIntent.modify(pid, metadata={
+#             'bag': json.dumps(request.session.get('bag', {})),
+#             'save_info': request.POST.get('save_info'),
+#             'username': request.user,
+#         })
+#         print('cache_checkout_data')
+#         return HttpResponse(status=200)
+#     except Exception as error:
+#         messages.error(request, 'Sorry, your payment cannot be processed \
+#             right now. Please try again later.')
+#         return HttpResponse(content=error, status=400)
 
-@require_POST
-def cache_checkout_data(request):
-    try:
-        # get payment intent id
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        # set up stripe
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        # tell stripe what we want to modify
-        stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
-            'username': request.user,
-        })
-        print('cache_checkout_data')
-        return HttpResponse(status=200)
-    except Exception as error:
-        messages.error(request, 'Sorry, your payment cannot be processed \
-            right now. Please try again later.')
-        return HttpResponse(content=error, status=400)
 
+@login_required
 def checkout(request):
+    """Get infos from the order form and create an order in the database"""
+
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-
     if request.method == 'POST':
         bag = request.session.get('bag', {})
-
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -66,16 +69,16 @@ def checkout(request):
                 try:
                     ticket = Ticket.objects.get(id=item_id)
                     order_line_item = OrderLineItem(
-                        order = order,
-                        ticket = ticket,
-                        quantity = item_data,
+                        order=order,
+                        ticket=ticket,
+                        quantity=item_data,
                     )
                     order_line_item.save()
                 except Ticket.DoesNotExist:
                     messages.error(request, (
                         "One or more of the tickets in your bag couldn't be \
                             found in our database. Please contact us!")
-                            )
+                    )
                     order.delete()
                     print('order deleted')
                     return redirect(reverse('view_bag'))
@@ -106,9 +109,9 @@ def checkout(request):
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-            amount = stripe_total,
-            currency = settings.STRIPE_CURRENCY,
-            payment_method_types = ['card']
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            payment_method_types=['card']
         )
 
         # Attempt to prefill the from with info from the user's profile
@@ -130,13 +133,12 @@ def checkout(request):
         else:
             order_form = OrderForm
 
-
         template = "checkout/checkout.html"
         context = {
             "order_form": order_form,
             'stripe_public_key': stripe_public_key,
             'client_secret': intent.client_secret,
-            'redirect': redirect,
+            # 'redirect': redirect,
 
 
         }
@@ -145,32 +147,30 @@ def checkout(request):
         return render(request, template, context)
 
 
+@login_required
 def checkout_success(request, order_number):
     """Handle successfull checkouts"""
     save_info = request.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    profile = UserProfile.objects.get(user=request.user)
+    order.user_profile = profile
+    order.save()
     print('order processed')
 
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach the user's profile to the order
-        order.user_profile = profile
-        order.save()
-
-        # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_email': order.email,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_city': order.city,
-                'default_postcode': order.postcode,
-                'default_state': order.state,
-                'default_country': order.country,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
+    # Save the user's info
+    if save_info:
+        profile_data = {
+            'default_email': order.email,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_city': order.city,
+            'default_postcode': order.postcode,
+            'default_state': order.state,
+            'default_country': order.country,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
 
     messages.success(request, f'Your order {order_number} has been \
         successfully processed. We will send a confirmation email to \
