ee68025beffa69f5ad41adaa764246946be909f5
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-02-06 17:37:55 +0100

    feat: Add user profile admin class

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """User Profile Admin"""
+    list_display = (
+        'user',
+        'street_address1',
+        'street_address2',
+        'postcode',
+        'city',
+    )
+
+
+admin.site.register(UserProfile)
