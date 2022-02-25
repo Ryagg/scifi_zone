edb064bd45ef710601503c9e080fa486390afca8
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-02-25 13:51:59 +0100

    refactor: Format code, add jshint comment
    Update pipfile.lock

--- conftest.py
+++ conftest.py
@@ -1,31 +1,36 @@
 # pylint: disable=W, C, R, no-member
+from pathlib import Path
 import pytest
 
 from django.contrib.auth.models import User
 from checkout.models import Order
 from tickets.models import Ticket, Actor
 
+
 @pytest.fixture
 def user_data():
-	return {
-    'email': 'user_email',
-    'username': 'user_name',
-    'password': 'user_password12345'
+    return {
+        'email': 'user_email',
+        'username': 'user_name',
+        'password': 'user_password12345'
     }
+
 
 @pytest.fixture
 def create_test_user(user_data):
-	test_user = User.objects.create_user(**user_data)
-	test_user.set_password(user_data.get('password'))
-	return test_user
+    test_user = User.objects.create_user(**user_data)
+    test_user.set_password(user_data.get('password'))
+    return test_user
+
 
 @pytest.fixture
 def authenticated_user(client, user_data):
-	test_user = User.objects.create_user(**user_data)
-	test_user.set_password(user_data.get('password'))
-	test_user.save()
-	client.login(**user_data)
-	return test_user
+    test_user = User.objects.create_user(**user_data)
+    test_user.set_password(user_data.get('password'))
+    test_user.save()
+    client.login(**user_data)
+    return test_user
+
 
 @pytest.fixture
 def create_ticket():
@@ -37,6 +42,7 @@ def create_ticket():
     )
     return ticket
 
+
 @pytest.fixture
 def order_data():
     order = Order.objects.create(
@@ -47,26 +53,36 @@ def order_data():
     )
     return order
 
+
 @pytest.fixture
 def actor_data():
     actors = Actor.objects.all()
     return actors
+
 
 @pytest.fixture
 def ticket_data():
     tickets = Ticket.objects.filter(name__icontains='Ticket')
     return tickets
 
+
 @pytest.fixture
 def package_data():
     packages = Ticket.objects.filter(name__icontains='Package')
     return packages
 
+
 @pytest.fixture
 def create_guest():
     new_guest = Actor.objects.create(
-        name = "Awesome Actor",
-        star_autograph_category = "A",
-        star_photoshoot_category = "A",
+        name="Awesome Actor",
+        star_autograph_category="A",
+        star_photoshoot_category="A",
+        star_image="image.jpg",
+        star_info_1="scifi info",
+        star_info_2="more info",
+        star_imdb="https://imdb.com/",
+        star_wiki="https://en.wikipedia.org",
+        star_official="https://google.com"
     )
     return new_guest
